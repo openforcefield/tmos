a8cfde77fd4b1548d41b5c83b5feed86f6b60dd4
jaclark5 <jennifer.clark@openforcefield.org>
2025-08-01 11:43:29 -0400

    add check for openbabel command line tool and install for readthedocs

--- tmos/__init__.py
+++ tmos/__init__.py
@@ -1,7 +1,12 @@
 """Python script to determine the oxidation state of a metal complex"""
 
 # Add imports here
+import shutil
 from .tmos import *
 
+from ._version import __version__ as __version__
 
-from ._version import __version__
+if shutil.which("obabel") is None:
+    raise ImportError(
+        "The Open Babel executable 'obabel' is not installed or not found in PATH. Please install Open Babel to proceed."
+    )
